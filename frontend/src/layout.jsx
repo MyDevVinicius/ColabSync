49b785e542e2516f746a9a7c957384d51ed59cdc
Vinicius Nascimento | HP Empresarial <nascimentovrn@outlook.com>
2025-06-23 16:38:05 -0400

    Update

--- frontend/src/layout.jsx
+++ frontend/src/layout.jsx
@@ -5,10 +5,10 @@ const Layout = ({ children }) => {
   return (
     <>
       <div className="flex flex-row gap-3  ">
-        <div className="basis-1/6.8  h-screen rounded-2xl flex justify-center ml-3 ">
+        <div className="basis-1/6.8  h-[98vh] rounded-2xl flex justify-center ml-3 mt-2 ">
           <Sidebar />
         </div>
-        <div className="basis-2/2 bg-slate-100 rounded-2xl flex p-4 mr-3 mt-2">
+        <div className="basis-2/2 bg-slate-100 rounded-2xl flex p-4 mr-3 mt-2 shadow-md">
           {children}
         </div>
       </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Layout from "../layout";
 const Home = () => {
   return (
     <Layout>
-      <h1 className="text-2xl font-bold">Conteudo Home</h1>
+      <div></div>
     </Layout>
   );
 };
